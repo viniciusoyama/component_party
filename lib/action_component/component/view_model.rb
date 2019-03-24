71dbf54a4d3f8bce112e72e93fc27b62215daaea
Vinicius Oyama <me@viniciusoyama.com>
2019-03-24 01:30:09 -0300

    feat(): ViewModel generating methods baseed on initialization data

--- lib/action_component/component/view_model.rb
+++ lib/action_component/component/view_model.rb
@@ -3,6 +3,15 @@ module ActionComponent
   class Component
     class ViewModel
       def initialize(**args)
+        generate_methods_from_hash(args)
+      end
+
+      private def generate_methods_from_hash(hash)
+        hash.each do |key, val|
+          define_singleton_method key.to_sym do
+            val
+          end
+        end
       end
     end
   end
---
+++ spec/action_component/component/view_model_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe ActionComponent::Component::ViewModel do
+  it "generates a method for each argument" do
+    vm = described_class.new(argument1: 'test1', argument2: 'test2')
+    expect(vm.argument1).to eq('test1')
+    expect(vm.argument2).to eq('test2')
+  end
+end
