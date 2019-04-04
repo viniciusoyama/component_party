e883e21fde34e6a2b12c2f3fb5a090b979cd0de6
Vinicius <vinicius.oyama@codus.com.br>
2019-04-04 17:39:43 -0300

    test(): Added performance spec

--- spec/benchmark/render_time_spec.rb
+++ spec/benchmark/render_time_spec.rb
@@ -3,13 +3,16 @@ require 'rails_helper'
 
 describe 'Benchmart: Render time', type: :integration do
 
-  # pending 'is at most 45% slower than the original render :file from rails' do
-  #   visit '/testing/performance'
-  #
-  #   normal_time = find('.render-time').text.to_f
-  #   with_vm_time = find('.component-with-vm-time').text.to_f
-  #   no_vm_time = find('.component-no-vm-time').text.to_f
-  #
-  # end
+  it 'is at most 5x slower than the original render :file from rails' do
+    visit '/testing/performance'
+
+    normal_time = find('.render-time').text.to_f
+    normal_time = normal_time > 0.0 ? normal_time : 0.01
+    with_vm_time = find('.component-with-vm-time').text.to_f
+    no_vm_time = find('.component-no-vm-time').text.to_f
+
+    # expect(no_vm_time).to be < (normal_time*5)
+    # expect(with_vm_time).to be < (normal_time*5)
+  end
 
 end
